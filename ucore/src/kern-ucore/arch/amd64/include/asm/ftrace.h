ba72f63eb00318e93efa02066c4f80cf3f8120b3
Tianyang Li <ty@li-tianyang.com>
2013-05-24 22:37:34 +0800

    mcount needs to store registers
    TODO: make mcount more like linux's

---
+++ ucore/src/kern-ucore/arch/amd64/include/asm/ftrace.h
@@ -0,0 +1,33 @@
+#ifndef _ASM_X86_FTRACE_H
+#define _ASM_X86_FTRACE_H
+
+/*
+ * taken from linux 2.6.34.14
+ *
+ * arch/x86/include/asm/ftrace.h
+ */
+
+.macro MCOUNT_SAVE_FRAME
+/* taken from glibc */
+subq $0x38, %rsp
+movq %rax, (%rsp)
+movq %rcx, 8(%rsp)
+movq %rdx, 16(%rsp)
+movq %rsi, 24(%rsp)
+movq %rdi, 32(%rsp)
+movq %r8, 40(%rsp)
+movq %r9, 48(%rsp)
+.endm
+
+.macro MCOUNT_RESTORE_FRAME
+movq 48(%rsp), %r9
+movq 40(%rsp), %r8
+movq 32(%rsp), %rdi
+movq 24(%rsp), %rsi
+movq 16(%rsp), %rdx
+movq 8(%rsp), %rcx
+movq (%rsp), %rax
+addq $0x38, %rsp
+.endm
+
+#endif /* _ASM_X86_FTRACE_H */
--- ucore/src/kern-ucore/arch/amd64/kernel/test_ftrace.c
+++ ucore/src/kern-ucore/arch/amd64/kernel/test_ftrace.c
@@ -1,6 +1,8 @@
 #include <kio.h>
 
 void test_ftrace_0() {
-	kprintf("in mcount\n");
+	static int i = 0;
+	kprintf("in mcount: %d\n", i);
+	i++;
 }
 

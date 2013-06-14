9f4e1ed8f1b5dcb1dfe6bc3e18e9467ee53c990c
Tianyang Li <ty@li-tianyang.com>
2013-06-15 01:46:53 +0800

    %%% sync

--- ucore/src/kern-ucore/arch/amd64/kernel/no_mcount/test_ftrace.c
+++ ucore/src/kern-ucore/arch/amd64/kernel/no_mcount/test_ftrace.c
@@ -2,18 +2,16 @@
 
 void test_ftrace_enter_0() {
 	static int i = 0;
-	if (i % 100 == 0) {
+	if (i % 1000 == 0) {
 		kprintf("enter mcount: %d\n", i);
-		i = 0;
 	}
 	i++;
 }
 
 void test_ftrace_exit_0() {
 	static int i = 0;
-	if (i % 100 == 0) {
+	if (i % 1000 == 0) {
 		kprintf("exit mcount: %d\n", i);
-		i = 0;
 	}
 	i++;
 }
